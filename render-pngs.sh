a230fe0dff45415572c466446afdbaa508048ee9
zagortenay333 <zagortenay333@users.noreplay.github.com>
2015-09-08 17:32:40 +0200

    updated render-pngs.sh

--- render-pngs.sh
+++ render-pngs.sh
@@ -1,52 +1,72 @@
 #! /bin/bash
 
+#======================================
+#   Global Variables
+#======================================
 INKSCAPE="/usr/bin/inkscape"
+SOURCE="icons.svg"
+SOURCE_TEMP="/tmp/icons.svg"
+DEFAULT_COLOR="#4078c0"
+INDEX="index.txt"
 
-#--------------------------------------------------
 
+#======================================
+#   User Input
+#======================================
 echo "Enter icon size (this is the width and height): "
 read SIZE
-echo "Enter icon-color (leave empty for default): "
-read ICON_COLOR
+echo "Enter 1 or more colors (space or tab separated): "
+read -a ICON_COLORS
 
-#--------------------------------------------------
 
-ICONS_DIR="PNG-"$SIZE
-SOURCE="icons.svg"
-DEFAULT_COLOR="#4078c0"
-
-if [ -z "$ICON_COLOR" ]; then
-    ICON_COLOR=$DEFAULT_COLOR
-fi
-
-#--------------------------------------------------
-
-if [ ! -d $ICONS_DIR ]; then
-    mkdir $ICONS_DIR
-fi
-
-#--------------------------------------------------
-
-SOURCE_TEMP="/tmp/"$SOURCE
-
-trap "rm $SOURCE_TEMP; exit" EXIT
-
-cp $SOURCE $SOURCE_TEMP
-
-sed -i "s/$DEFAULT_COLOR/$ICON_COLOR/" $SOURCE_TEMP
-
-for i in `cat index.txt`
+#======================================
+#   Loop over icon colors
+#======================================
+for color in ${ICON_COLORS[*]}
 do
-if [ -f $ICONS_DIR/$i.png ]; then
-        echo $ICONS_DIR/$i.png exists.
-else
-    echo
-    echo Rendering $ICONS_DIR/$i.png
-    $INKSCAPE --export-id="$i" \
-              --export-id-only \
-              --export-width=$SIZE \
-              --export-height=$SIZE \
-              --export-png=$ICONS_DIR/$i.png $SOURCE_TEMP >/dev/null
-fi
+
+# Create dir with color name
+    if [ ! -d $color ] && [ ! -z "$ICON_COLORS" ]; then
+        mkdir $color
+    else
+        mkdir $DEFAULT_COLOR
+    fi
+
+
+# Trap copy
+    trap "rm $SOURCE_TEMP; exit" INT TERM
+
+
+# Make a temp copy of SOURCE
+    cp $SOURCE $SOURCE_TEMP
+
+
+# Change color of temp copy
+    if [ ! -z "ICON_COLORS" ]; then
+        sed -i "s/$DEFAULT_COLOR/$color/" $SOURCE_TEMP
+    fi
+
+
+# Loop over SVG folder & render png's
+    while read i; do
+        i2=${i##*/}  i2=${i2%.*}
+
+        if [ -f $color/$i2.png ]; then
+                echo $color/$i2.png exists.
+        else
+            echo
+            echo Rendering $color/$i2.png
+            $INKSCAPE --export-id=$i \
+                      --export-id-only \
+                      --export-width=$SIZE \
+                      --export-height=$SIZE \
+                      --export-png=$color/$i2.png $SOURCE_TEMP >/dev/null
+        fi
+    done < $INDEX
+
+
+# Remove copy before next iteration or EXIT
+    rm $SOURCE_TEMP
+
 done
 exit 0
