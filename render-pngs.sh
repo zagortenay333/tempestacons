3c9994acb608cbea9fc0cf3e5d1539e8487e8dbb
zagortenay333 <krionubis@gmail.com>
2016-02-17 00:15:42 +0100

    update render-pngs.sh

--- render-pngs.sh
+++ render-pngs.sh
@@ -7,6 +7,7 @@ INKSCAPE="/usr/bin/inkscape"
 SOURCE="icons.svg"
 SOURCE_TEMP="/tmp/icons.svg"
 DEFAULT_COLOR="#4078c0"
+DEFAULT_SIZE=32
 INDEX="index.txt"
 
 
@@ -19,12 +20,10 @@ echo "Enter 1 or more colors (space or tab separated): "
 read -r -a ICON_COLORS
 
 
-# If SIZE given, use for width & height
-# Otherwise, default dimensions used
-if [ ! -z "$SIZE" ]; then
-    SIZE="--export-width=$SIZE --export-height=$SIZE"
+# If no colors given, add default color to array
+if [ -z "$SIZE" ]; then
+    SIZE=$DEFAULT_SIZE
 fi
-
 
 # If no colors given, add default color to array
 if [ ${#ICON_COLORS[*]} -eq 0 ]; then
@@ -37,8 +36,8 @@ fi
 #======================================
 for color in ${ICON_COLORS[*]}; do
 
-    # Create dir with color name
-    mkdir -p "$color"
+    # Create dir with color & size in
+    mkdir -p "$color--$SIZE"
 
 
     # Trap copy
@@ -57,15 +56,15 @@ for color in ${ICON_COLORS[*]}; do
 
     # Loop through index.txt & render png's
     while read -r i; do
-        if [ -f "$color/$i.png" ]; then
-            echo "$color/$i.png" exists.
+        if [ -f "$color--$SIZE/$i.png" ]; then
+            echo "$color--$SIZE/$i.png" exists.
         else
             echo
-            echo Rendering "$color/$i.png"
+            echo Rendering "$color--$SIZE/$i.png"
             "$INKSCAPE" --export-id="$i" \
                         --export-id-only \
-                        "$SIZE" \
-                        --export-png="$color/$i.png" "$SOURCE_TEMP" >/dev/null
+                        --export-width="$SIZE" --export-height="$SIZE" \
+                        --export-png="$color--$SIZE/$i.png" "$SOURCE_TEMP" >/dev/null
         fi
     done < "$INDEX"
 
